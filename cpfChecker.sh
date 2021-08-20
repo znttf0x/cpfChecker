b5d1d9afe2e420a3c03fb40d611f670207348341
znttf0x <32802617+znttf0x@users.noreply.github.com>
2021-08-20 01:46:05 -0300

    Update cpfChecker.sh

--- cpfChecker.sh
+++ cpfChecker.sh
@@ -21,7 +21,7 @@ fi
 
 # Program
 format=$(echo $1 | sed 's/\.//g' | sed 's/-//g')
-read -a cpf <<< $(echo "$1" | sed 's/./& /g')
+read -a cpf <<< $(echo "$format" | sed 's/./& /g')
 
 stSum=$(((${cpf[0]}*10)+(${cpf[1]}*9)+(${cpf[2]}*8)+(${cpf[3]}*7)+(${cpf[4]}*6)+(${cpf[5]}*5)+(${cpf[6]}*4)+(${cpf[7]}*3)+(${cpf[8]}*2)))
 ndSum=$(((${cpf[0]}*11)+(${cpf[1]}*10)+(${cpf[2]}*9)+(${cpf[3]}*8)+(${cpf[4]}*7)+(${cpf[5]}*6)+(${cpf[6]}*5)+(${cpf[7]}*4)+(${cpf[8]}*3)+(${cpf[9]}*2)))
